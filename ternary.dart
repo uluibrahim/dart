d618cc94cd6e6785a4c8c45df67c1fda07be3f63
uluibrahim <62259512+uluibrahim@users.noreply.github.com>
2020-04-25 16:52:10 +0300

    Update ternary.dart

--- ternary.dart
+++ ternary.dart
@@ -4,10 +4,10 @@ main(List<String> args) {
   var kucukSayi;
   //1.tarz
   sayi1<sayi2 ? kucukSayi=sayi1 : kucukSayi=sayi2;
-  print(kucukSayi);
+  print(kucukSayi); // cikti : 4
   //2.tarz
   kucukSayi=sayi1<sayi2 ? sayi1:sayi2;
-  print(kucukSayi);
+  print(kucukSayi); // cikti : 4
   String isim=" ibrahim";
   String soyIsim="ULU";
   String mesaj;
